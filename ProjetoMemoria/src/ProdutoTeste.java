0b48fca96f95b166b580814efc7dae9fa18b290c
TalesSilva <tales.silva@gafit.com.br>
2022-08-11 23:44:39 -0300

    Final semana 1

---
+++ ProjetoMemoria/src/ProdutoTeste.java
@@ -0,0 +1,13 @@
+
+public class ProdutoTeste {
+
+	public static void main(String[] args) {
+
+		Produto p1, p2;
+		
+		p1 = new Produto(123, "Computador", 4300.00);
+		
+		System.out.println(p1.Exibir());	
+	}
+	
+}
--- ProjetoTime/src/app/TimeTeste.java
+++ ProjetoTime/src/app/TimeTeste.java
@@ -1,3 +1,5 @@
+package app;
+import core.Time;
 
 public class TimeTeste {
 
