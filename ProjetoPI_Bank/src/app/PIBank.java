e3b15d2cd1f5833356280e6bd99b81b41c531503
TalesSilva <tales.silva@gafit.com.br>
2022-08-21 12:04:21 -0300

    polimorfismo

---
+++ ProjetoPI_Bank/src/app/PIBank.java
@@ -0,0 +1,15 @@
+package app;
+
+import model.Conta;
+
+public class PIBank {
+
+	public static void main(String[] args) {
+
+		Conta c1 = new Conta("Isídro", "123.456.789-00", 3526, 13520);
+		
+		System.out.println(c1);
+		
+	}
+
+}
