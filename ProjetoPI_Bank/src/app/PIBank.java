159c3d4efd4b2db67eb8dd0ad8b216ecc33172d3
TalesSilva <tales.silva@gafit.com.br>
2022-08-21 12:12:31 -0300

    polimorfismo pt2

--- ProjetoPI_Bank/src/app/PIBank.java
+++ ProjetoPI_Bank/src/app/PIBank.java
@@ -1,15 +1,19 @@
 package app;
 
 import model.Conta;
+import model.ContaEspecial;
 
 public class PIBank {
 
 	public static void main(String[] args) {
 
-		Conta c1 = new Conta("Isídro", "123.456.789-00", 3526, 13520);
+		Conta c1 = new Conta("Isídro", "123.456.789-00", 3526, 100.00);
+		Conta c2 = new ContaEspecial("Julio", "987.654.321-00", 2622, 100.00, 100.00);
 		
+		c1.debitar(80);
+		c2.debitar(180);
 		System.out.println(c1);
-		
+		System.out.println(c2);
 	}
 
 }
